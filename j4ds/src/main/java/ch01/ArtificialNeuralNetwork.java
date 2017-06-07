f45db70735c72a6348a18779d7756d3993dc7fee
Yauhen <stan01@tut.by>
2017-06-07 21:59:16 +0300

    chapter 01

---
+++ j4ds/src/main/java/ch01/ArtificialNeuralNetwork.java
@@ -0,0 +1,45 @@
+package ch01;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+
+import java.io.FileReader;
+
+/**
+ * Created by Aspire on 07.06.2017.
+ */
+public class ArtificialNeuralNetwork {
+    public static void main(String[] args) {
+        mlp();
+    }
+
+    static void mlp(){
+        String trainingFileName = "com/src/main/resources/dermatologyTrainingSet.arff";
+        String testingFileName = "com/src/main/resources/dermatologyTestingSet.arff";
+
+        try (
+                FileReader trainingReader = new FileReader(trainingFileName);
+                FileReader testingReader = new FileReader(testingFileName)
+        ) {
+            Instances trainingInstances = new Instances(trainingReader);
+            trainingInstances.setClassIndex(trainingInstances.numAttributes() - 1);
+            Instances testingInstances = new Instances(testingReader);
+            testingInstances.setClassIndex(testingInstances.numAttributes() - 1);
+
+            MultilayerPerceptron mlp = new MultilayerPerceptron();
+            mlp.setLearningRate(0.1);
+            mlp.setMomentum(0.2);
+            mlp.setTrainingTime(2000);
+            mlp.setHiddenLayers("3");
+            mlp.buildClassifier(trainingInstances);
+
+            Evaluation evaluation = new Evaluation(trainingInstances);
+            evaluation.evaluateModel(mlp, testingInstances);
+
+            System.out.println(evaluation.toSummaryString());
+        } catch (Exception ex) {
+            // Handle exceptions
+        }
+    }
+}
---
+++ j4ds/src/main/java/ch01/Statistic.java
@@ -0,0 +1,24 @@
+package ch01;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.SynchronizedDescriptiveStatistics;
+
+/**
+ * Created by Aspire on 07.06.2017.
+ */
+public class Statistic {
+    public static void main(String[] args) {
+        findMedian();
+    }
+
+    static void findMedian(){
+        double[] testData = {12.5, 18.3, 11.2, 19.0, 22.1, 14.3, 16.2, 12.5, 17.8, 16.5, 12.5};
+        DescriptiveStatistics statTest = new SynchronizedDescriptiveStatistics();
+
+        for(double num : testData){
+            statTest.addValue(num);
+        }
+
+        System.out.println("The median is " + statTest.getPercentile(50));
+    }
+}
