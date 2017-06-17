39e6a4a4c4a8fbc8d92c403cd71550aae63c0a4d
Yauhen <stan01@tut.by>
2017-06-17 13:16:33 +0300

    handling PDF

---
+++ src/main/java/ch03Cleaning/PdfHandler.java
@@ -0,0 +1,23 @@
+package ch03Cleaning;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Aspire on 17.06.2017.
+ */
+public class PdfHandler {
+    public static void main(String[] args) {
+        try {
+            PDDocument document = PDDocument.load(new File("src/main/resources/PDF File.pdf"));
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
