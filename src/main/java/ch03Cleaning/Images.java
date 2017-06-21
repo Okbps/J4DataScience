626a9c0550b9112deb83b60b8cc363702e9bd215
Yauhen <stan01@tut.by>
2017-06-21 21:33:28 +0300

    Cleaning images prerequisites

---
+++ src/main/java/ch03Cleaning/Images.java
@@ -0,0 +1,11 @@
+package ch03Cleaning;
+
+/**
+ * Created by Aspire on 21.06.2017.
+ */
+public class Images {
+    public static void main(String[] args) {
+        Images images = new Images();
+
+    }
+}
