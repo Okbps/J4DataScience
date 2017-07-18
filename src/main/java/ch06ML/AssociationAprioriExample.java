dc4e0210e8314609246fb249cc6351dd18e2aad4
Yauhen <stan01@tut.by>
2017-07-18 20:54:57 +0300

    Association rule for purchased items, apriori algorithm

---
+++ src/main/java/ch06ML/AssociationAprioriExample.java
@@ -0,0 +1,25 @@
+package ch06ML;
+
+import weka.associations.Apriori;
+import weka.core.Instances;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static util.Globals.RESOURCE_FOLDER;
+
+public class AssociationAprioriExample {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(RESOURCE_FOLDER + "babies.arff"));
+        Instances data = new Instances(br);
+        br.close();
+
+        Apriori apriori = new Apriori();
+        apriori.setNumRules(100);
+        apriori.setMinMetric(0.5);
+
+        apriori.buildAssociations(data);
+        System.out.println(apriori);
+    }
+}
