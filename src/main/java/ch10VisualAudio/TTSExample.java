cba1552efbbf3cfb4fe8ad171a587e07dca14a3a
Yauhen <stan01@tut.by>
2017-09-03 16:26:19 +0300

    mbrola text to speech example

--- src/main/java/ch10VisualAudio/TTSExample.java
+++ src/main/java/ch10VisualAudio/TTSExample.java
@@ -1,31 +1,73 @@
 package ch10VisualAudio;
 
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
+import java.beans.PropertyVetoException;
+import java.util.Locale;
 
-// Text-to-speech
+import javax.speech.AudioException;
+import javax.speech.Central;
+import javax.speech.EngineException;
+import javax.speech.EngineStateError;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+import javax.speech.synthesis.Voice;
+
 public class TTSExample {
-    VoiceManager freettsVM;
-    Voice freettsVoice;
+    SynthesizerModeDesc desc;
+    Synthesizer synthesizer;
+    Voice voice;
 
-    public TTSExample(String words) {
-        // Most important part!
-        System.setProperty("mbrola.base", "D:\\mbrola");
-        freettsVM = VoiceManager.getInstance();
+    public void init(String voiceName) throws EngineException, AudioException,
+            EngineStateError, PropertyVetoException {
+        if (desc == null) {
+            //default
+//			System.setProperty("freetts.voices",
+//					"com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
 
-        // Simply change to MBROLA voice
-        freettsVoice = freettsVM.getVoice("mbrola_us1");
-
-        // Allocate your chosen voice
-        freettsVoice.allocate();
-        sayWords(words);
+            //have to be setup
+            System.setProperty("freetts.voices", "de.dfki.lt.freetts.en.us.MbrolaVoiceDirectory");
+            desc = new SynthesizerModeDesc(Locale.US);
+            Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+            synthesizer = Central.createSynthesizer(desc);
+            synthesizer.allocate();
+            synthesizer.resume();
+            SynthesizerModeDesc smd = (SynthesizerModeDesc) synthesizer
+                    .getEngineModeDesc();
+            Voice[] voices = smd.getVoices();
+            Voice voice = null;
+            for (int i = 0; i < voices.length; i++) {
+                if (voices[i].getName().equals(voiceName)) {
+                    voice = voices[i];
+                    break;
+                }
+            }
+            synthesizer.getSynthesizerProperties().setVoice(voice);
+        }
     }
 
-    public void sayWords(String words) {
-        // Make her speak!
-        freettsVoice.speak(words);
+    public void terminate() throws EngineException, EngineStateError {
+        synthesizer.deallocate();
     }
 
-    public static void main(String [] args) {
-        new TTSExample("Hello there! Now M BROLA and Free T T S work together!");
-    }}
+    public void doSpeak(String speakText) throws EngineException,
+            AudioException, IllegalArgumentException, InterruptedException {
+        synthesizer.speakPlainText(speakText, null);
+        synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("mbrola.base", "d:\\mbrola");
+        TTSExample su = new TTSExample();
+
+        //have to be setup on your env
+        su.init("mbrola_us1");
+
+        //default
+        //su.init("kevin16");
+        //su.init("kevin");
+        //su.doSpeak("Hello world!");
+        su.doSpeak(SAMPLE);
+        su.terminate();
+    }
+
+    final static String SAMPLE = "Wiki said, Floyd Mayweather, Jr. is an American professional boxer. He is currently undefeated as a professional and is a five-division world champion, having won ten world titles and the lineal championship in four different weight classes";
+}
