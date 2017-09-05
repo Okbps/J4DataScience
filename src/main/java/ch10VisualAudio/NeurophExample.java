c4ea825b4d3e9c0c0f8dec8e1fbaf5bf448649db
Yauhen <stan01@tut.by>
2017-09-05 22:15:36 +0300

    image recognition using Neuroph trained model

---
+++ src/main/java/ch10VisualAudio/NeurophExample.java
@@ -0,0 +1,27 @@
+package ch10VisualAudio;
+
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.imgrec.ImageRecognitionPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static util.Globals.RESOURCE_FOLDER;
+
+public class NeurophExample {
+    public static void main(String[] args) {
+        NeuralNetwork iRNet = NeuralNetwork.createFromFile(RESOURCE_FOLDER + "faces_net.nnet");
+        ImageRecognitionPlugin iRFile = (ImageRecognitionPlugin) iRNet.getPlugin(ImageRecognitionPlugin.class);
+
+        try{
+            HashMap<String, Double> faceMap =
+                    iRFile.recognizeImage(new File(RESOURCE_FOLDER + "pic/oval-face.jpg"));
+
+            System.out.println(faceMap);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
