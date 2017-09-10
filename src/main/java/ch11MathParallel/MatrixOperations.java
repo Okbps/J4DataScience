cdb3b02fb7ddada68c344a79a273605b57ac5c0c
Yauhen <stan01@tut.by>
2017-09-10 11:57:29 +0300

    nd4j matrix multiplication

--- src/main/java/ch11MathParallel/MatrixOperations.java
+++ src/main/java/ch11MathParallel/MatrixOperations.java
@@ -1,37 +1,40 @@
 package ch11MathParallel;
 
 import org.jblas.DoubleMatrix;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 public class MatrixOperations {
+    private double matrixA[][] = {
+            {0.1950, 0.0311},
+            {0.3588, 0.2203},
+            {0.1716, 0.5931},
+            {0.2105, 0.3242}
+    };
+
+    private double matrixB[][] = {
+            {0.0502, 0.9823, 0.9472},
+            {0.5732, 0.2694, 0.916}
+    };
+
     public static void main(String[] args) {
         MatrixOperations mo = new MatrixOperations();
-//        mo.multiply();
+        mo.multiply();
         mo.multiplyJblas();
+        mo.multiplyNd4j();
     }
 
-    void multiply(){
+    private void multiply(){
         int n = 4;
         int m = 2;
         int p = 3;
-
-        double A[][] = {
-                {0.1950, 0.0311},
-                {0.3588, 0.2203},
-                {0.1716, 0.5931},
-                {0.2105, 0.3242}
-        };
-
-        double B[][] = {
-                {0.0502, 0.9823, 0.9472},
-                {0.5732, 0.2694, 0.916}
-        };
 
         double C[][] = new double[n][p];
 
         for (int i = 0; i < n; i++) {
             for (int k = 0; k < m; k++) {
                 for (int j = 0; j < p; j++) {
-                    C[i][j] += A[i][k] * B[k][j];
+                    C[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
@@ -45,23 +48,28 @@ public class MatrixOperations {
         }
     }
 
-    void multiplyJblas(){
-        DoubleMatrix A = new DoubleMatrix(new double[][]{
-                {0.1950, 0.0311},
-                {0.3588, 0.2203},
-                {0.1716, 0.5931},
-                {0.2105, 0.3242}
-        });
-
-        DoubleMatrix B = new DoubleMatrix(new double[][]{
-                {0.0502, 0.9823, 0.9472},
-                {0.5732, 0.2694, 0.916}
-        });
+    private void multiplyJblas(){
+        DoubleMatrix A = new DoubleMatrix(matrixA);
+        DoubleMatrix B = new DoubleMatrix(matrixB);
 
         DoubleMatrix C = A.mmul(B);
 
         for (int i = 0; i < C.getRows(); i++) {
             System.out.println(C.getRow(i));
+        }
+    }
+
+    private void multiplyNd4j(){
+        double[] A = {0.1950, 0.0311, 0.3588, 0.2203, 0.1716, 0.5931, 0.2105, 0.3242};
+        double[] B = {0.0502, 0.9823, 0.9472, 0.5732, 0.2694, 0.916};
+
+        INDArray aINDArray = Nd4j.create(A, new int[]{4, 2}, 'c');
+        INDArray bINDArray = Nd4j.create(B, new int[]{2, 3}, 'c');
+
+        INDArray cINDArray = aINDArray.mmul(bINDArray);
+
+        for (int i = 0; i < cINDArray.rows(); i++) {
+            System.out.println(cINDArray.getRow(i));
         }
     }
 }
