b02581467562495de2ea2c60ae6652942957c4e5
Yauhen <stan01@tut.by>
2017-09-12 22:54:24 +0300

    sentiment classification model training and storing

---
+++ src/main/java/ch12Altogether/SentimentAnalysisTrainingData.java
@@ -0,0 +1,78 @@
+package ch12Altogether;
+
+import com.aliasi.classify.Classification;
+import com.aliasi.classify.Classified;
+import com.aliasi.classify.DynamicLMClassifier;
+import com.aliasi.classify.LMClassifier;
+import com.aliasi.lm.NGramProcessLM;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static util.Globals.RESOURCE_FOLDER;
+
+public class SentimentAnalysisTrainingData {
+    private static final String MODEL_PATH = RESOURCE_FOLDER+"/txt_sentoken/model";
+
+    public static void main(String[] args) {
+        SentimentAnalysisTrainingData satd = new SentimentAnalysisTrainingData();
+        satd.buildModel();
+        SentimentAnalysisTrainingData.invokeModel();
+    }
+
+    private void buildModel(){
+        String[]labels = {"neg", "pos"};
+        int nGramSize = 8;
+
+        DynamicLMClassifier<NGramProcessLM> classifier =
+                DynamicLMClassifier.createNGramProcess(labels, nGramSize);
+
+        try {
+            List<String> lines = Files.readAllLines(
+                    Paths.get(RESOURCE_FOLDER + "csv/Sentiment Analysis Dataset.csv"),
+                    StandardCharsets.ISO_8859_1
+            );
+
+            int count = 0;
+            for (String s : lines) {
+                if (++count == 1) continue;
+
+                String[] oneLine = s.split(",");
+
+                Classification classification = new Classification(
+                        oneLine[1].equals("1") ? "pos" : "neg");
+
+                Classified<CharSequence> classified = new Classified<>(oneLine[3], classification);
+                classifier.handle(classified);
+
+//                if(count==100) break;
+                if(count%10000==0) System.out.println(count + "/" + lines.size() + " processed");
+            }
+
+        }catch (IOException e){
+
+        }
+
+
+        try(ObjectOutputStream os = new ObjectOutputStream(
+                new FileOutputStream(MODEL_PATH))
+        ){
+            classifier.compileTo(os);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static LMClassifier invokeModel(){
+        try(ObjectInput input = new ObjectInputStream(new FileInputStream(MODEL_PATH));
+        ){
+            return (LMClassifier) input.readObject();
+        } catch (IOException|ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
